8e46b53f4a8315c9c1582b0c9e5d2aa16ca5d6ec
Gregor von Laszewski <laszewski@gmail.com>
2015-10-22 10:23:34 -0400

    new: usr: simple django based portal framework for cloudmesh

---
+++ Makefile
@@ -0,0 +1,24 @@
+view:
+	open http://127.0.0.1:8000/
+
+migrate:
+	python manage.py migrate
+
+run: migrate
+	python manage.py runserver
+
+doc:
+	open http://127.0.0.1:8000/docs/
+
+
+######################################################################
+# CLEANING
+######################################################################
+
+clean:
+	# cd docs; make clean
+	rm -rf build dist docs/build .eggs *.egg-info
+	rm -rf *.egg-info
+	find . -name "*~" -exec rm {} \;
+	find . -name "*.pyc" -exec rm {} \;
+	echo "clean done"
---
+++ cloudmesh_portal/urls.py
@@ -0,0 +1,40 @@
+"""comet URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.flatpages.sitemaps import FlatPageSitemap
+from django.contrib.sitemaps.views import sitemap
+from .views import HomePageView, FormHorizontalView, FormInlineView, PaginationView, FormWithFilesView, \
+    DefaultFormView, MiscView, DefaultFormsetView, DefaultFormByFieldView
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
+    url(r'^pages/', include('django.contrib.flatpages.urls')),
+    # Remove the sitemap in production
+    url(r'^sitemap\.xml$', sitemap,
+        {'sitemaps': {'flatpages': FlatPageSitemap}},
+        name='django.contrib.sitemaps.views.sitemap'),
+        url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^formset$', DefaultFormsetView.as_view(), name='formset_default'),
+    url(r'^form$', DefaultFormView.as_view(), name='form_default'),
+    url(r'^form_by_field$', DefaultFormByFieldView.as_view(), name='form_by_field'),
+    url(r'^form_horizontal$', FormHorizontalView.as_view(), name='form_horizontal'),
+    url(r'^form_inline$', FormInlineView.as_view(), name='form_inline'),
+    url(r'^form_with_files$', FormWithFilesView.as_view(), name='form_with_files'),
+    url(r'^pagination$', PaginationView.as_view(), name='pagination'),
+    url(r'^misc$', MiscView.as_view(), name='misc'),
+]
