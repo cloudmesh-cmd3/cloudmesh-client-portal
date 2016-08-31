fad52a499b9f52c15fd8d904745fc8ce234c78b4
Gregor von Laszewski <laszewski@gmail.com>
2016-08-31 15:02:46 -0400

    new:dev: create a hello world

--- cloudmesh_portal/cloudmesh_portal_hpc/views.py
+++ cloudmesh_portal/cloudmesh_portal_hpc/views.py
@@ -4,6 +4,10 @@ import datetime
 from django_jinja import library
 
 
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
